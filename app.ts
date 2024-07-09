d2530cc4c3cc7b37feec405aece9d4760715ef26
Gabriele Lo Re <gabrolr70@gmail.com>
2024-07-09 09:55:22 +0200

    fix(site): express sintax

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import { Auth } from "./models/Auth";
 import { Favourite } from "./models/Favorite";
 import { Device } from "./models/Device";
 import { DocAPI } from "./doc/DocAPI";
-import Express from "express";
+import express, {Request, Response} from "express";
 
 class Marketplace {
   users:Array<User> = [];
@@ -615,6 +615,6 @@ const app = express();
 const server = express.json();
 app.use(server);
 
-app.get("/", function(req, res) {
+app.get("/", function(req: Request, res: Response) {
   res.send("Hello world");
 });
