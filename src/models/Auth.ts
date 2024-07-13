84a8d0d503df8c4cd8ee521b21aaedec2a21e1b5
BuildTools <unconfigured@null.spigotmc.org>
2024-07-13 15:10:35 +0200

    feat(app): token is now a random string

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -7,11 +7,23 @@ export class Auth {
     constructor(referenceKeyUser:User["primaryKey"]) {
       this.primaryKey = Math.random().toString();
       this.referenceKeyUser = referenceKeyUser;
-      let tok =  Math.random()*10000000;
+      let tok =  this.#generateToken(8);
       this.token = tok.toString();
     }
     getToken()
     {
       return this.token;
     }
+    #generateToken(lenght: number){
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      let counter = 0;
+      while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+      }
+      return result;
+  }
+  
   }
